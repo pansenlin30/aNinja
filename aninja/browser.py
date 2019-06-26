4a8a2c7095052c6d431b7924c079fc6d1e17e11f
zireael <zireael.me@gmail.com>
2019-06-26 23:02:21 +0800

    fix(client): split into two module

--- aninja/browser.py
+++ aninja/browser.py
@@ -1,29 +1,15 @@
 import asyncio
-import inspect
-import logging
 from io import BytesIO
-# typing
-from types import TracebackType
-from typing import Any, List, Optional, Tuple, Type, Union
+from typing import List, Optional
 
 import pyppeteer
-from aiohttp import ClientSession
 from PIL import Image
 from pyppeteer.page import Page
 
-import aninja.conf as conf
 from aninja.cookies import CookiesManager
 from aninja.utils import get_user_agent, pretend_js_list, random_delay
 
 _Page = Optional[Page]
-_Client = 'Client'
-_CSSSelector = str
-_TypeIn = Tuple[_CSSSelector, str]
-_URL = Union[str, 'URL']
-
-logger = logging.getLogger(__name__)
-
-DEFUALT_HEADERS = {'User-Agent': get_user_agent()}
 
 
 def patch_pyppeteer():
@@ -39,90 +25,6 @@ def patch_pyppeteer():
 
 
 patch_pyppeteer()
-
-
-class HTTPClient:
-    """A client uses aiohttp to make requests.
-    """
-
-    def __init__(self, cookies_manager=None, **kwargs):
-        self.cookies_manager: CookiesManager = cookies_manager if cookies_manager else CookiesManager()
-        self.session = None
-        headers = kwargs.pop('headers', None)
-        headers = headers if headers else DEFUALT_HEADERS
-
-        self.session = ClientSession(headers=headers, **kwargs)
-        self.cookies_manager.sync_to_aiohttp_session(self.session)
-
-        self.logger = logging.getLogger('HTTP')
-        self.level = self.logger.getEffectiveLevel()
-
-    async def close(self):
-        if self.session:
-            await self.session.close()
-        self.session = None
-
-    async def request(self,
-                      url: _URL,
-                      method: str,
-                      params=None,
-                      data=None,
-                      **kwargs: Any):
-        if self.level == logging.DEBUG:
-            func = inspect.stack()[2][3]
-            msg = conf.DEBUG_REQ_FMT % (func, url, method,
-                                        params, data)
-            self.logger.debug(msg)
-        resp = await self.session.request(method, url, params=params, data=data, **kwargs)
-        self.cookies_manager.update_from_aiohttp_session(self.session)
-        return resp
-
-    async def get(self, url: _URL, params=None, **kwargs: Any):
-        return await self.request(url, "GET", params=params, **kwargs)
-
-    async def post(self, url: _URL, data: Any = None, params=None, **kwargs: Any):
-        return await self.request(url, "POST", data=data, params=None, **kwargs)
-
-    async def send(self, request, expect_json=True, ignore_content=False):
-        r = await self.request(method=request.method,
-                               url=request.url,
-                               params=request.params,
-                               data=request.data,
-                               headers=request.headers)
-
-        await self._debug_response(r)
-        return r
-
-    async def _debug_response(self, resp):
-        if self.level == logging.DEBUG:
-            func = inspect.stack()[4][3]
-            msg = conf.DEBUG_RES_FMT % (func, resp.status, await resp.text())
-            self.logger.debug(msg)
-
-    async def check(self, check_flag: str, url: _URL = ""):
-        resp = await self.get(url)
-        if check_flag in await resp.text():
-            return True
-        return False
-
-    def __enter__(self) -> None:
-        raise TypeError("Use async with instead")
-
-    def __exit__(self,
-                 exc_type: Optional[Type[BaseException]],
-                 exc_val: Optional[BaseException],
-                 exc_tb: Optional[TracebackType]) -> None:
-        # __exit__ should exist in pair with __enter__ but never executed
-        pass  # pragma: no cover
-
-    async def __aenter__(self) -> 'HTTPClient':
-        return self
-
-    async def __aexit__(self,
-                        exc_type: Optional[Type[BaseException]],
-                        exc_val: Optional[BaseException],
-                        exc_tb: Optional[TracebackType]) -> None:
-        await self.close()
 
 
 class NinjaPage(Page):
