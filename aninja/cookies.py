87f9aaf30b87ec8e4ed2d2a7bdd10268d645914e
zireael <zireael.me@gmail.com>
2019-07-06 22:48:07 +0800

    feat(cookies): support never_expires

--- aninja/cookies.py
+++ aninja/cookies.py
@@ -2,21 +2,28 @@ import time
 import json
 from http.cookiejar import Cookie, LWPCookieJar
 from http.cookies import Morsel, SimpleCookie
+import time
 
-from aninja.utils import format_expires, filter_attrs, expires_to_number, expires_to_str
+from aninja.utils import (
+    format_expires,
+    filter_attrs,
+    expires_to_number,
+    expires_to_str,
+)
 from requests.cookies import RequestsCookieJar
 
 
 # Typing
-_Page = 'Page'
-_Cookie = 'Cookie'
+_Page = "Page"
+_Cookie = "Cookie"
 _Path = str
-_CookieJar = 'CookieJar'
+_CookieJar = "CookieJar"
 
 
 class NinjaCookieJar(LWPCookieJar, RequestsCookieJar):
     """:class:`requests.cookies.RequestsCookieJar` compatible
     :class:`cookielib.LWPCookieJar`"""
+
     pass
 
 
@@ -30,17 +37,21 @@ class CookiesManager:
     def __init__(self) -> None:
         self._jar = NinjaCookieJar()
 
-    def load(self, filename, ):
+    def load(self, filename):
         """Load cookies from the file :attr:`.API.cookies_filename`"""
         self._jar.load(filename, ignore_discard=True, ignore_expires=True)
 
-    def save(self, filename, ):
+    def save(self, filename):
         """Save cookies to the file"""
         self._jar.save(filename, ignore_discard=True, ignore_expires=True)
 
     def update(self, other):
         """updates with cookies from another CookieJar or dict-like, same as RequestsCookieJar"""
         self._jar.update(other)
+
+    def never_expires(self):
+        for cookie in self._jar:
+            cookie.expires = int(time.time()) + 50 * 365 * 24 * 3600
 
     def update_from_aiohttp_session(self, session) -> None:
         for morsel in session.cookie_jar:
@@ -49,9 +60,9 @@ class CookiesManager:
     async def update_from_pyppeteer(self, page: _Page) -> None:
         cookies_list = await page.cookies()
         for cookie_dict in cookies_list:
-            f = filter_attrs(time_format='number', **cookie_dict)
-            name = f.pop('name')
-            value = f.pop('value')
+            f = filter_attrs(time_format="number", **cookie_dict)
+            name = f.pop("name")
+            value = f.pop("value")
             cookie = create_cookie(name, value, **f)
             self._jar.set_cookie(cookie)
 
@@ -71,13 +82,26 @@ class CookiesManager:
                 await page.setCookie(cookie_dict)
 
     def output_header_string(self, domain=None, path=None) -> str:
-        return '; '.join(
-            [k+'='+v
-             for k, v in self.output_dict(domain, path).items()]
+        return "; ".join(
+            [k + "=" + v for k, v in self.output_dict(domain, path).items()]
         )
 
     def output_js(self, domain=None, path=None) -> str:
         return self.output_simplecookie(domain, path).js_output()
+
+    def output_js_function(self, domain=None, path=None) -> str:
+        func_str = "(()=>{"
+        for cookie_dict in self.output_detailed(domain, path):
+            name = cookie_dict.pop("name")
+            value = cookie_dict.pop("value")
+            func_str += 'document.cookie = "{}={}; '.format(name, value)
+
+            cookie = ["{}={}".format(a, b) for a, b in cookie_dict.items()]
+            append = '{}";\n'.format("; ".join(cookie))
+            func_str += append
+        func_str += "})();"
+
+        return func_str
 
     def output_dict(self, domain=None, path=None) -> dict:
         """returns a plain old Python dict of name-value pairs of cookies.
@@ -93,24 +117,26 @@ class CookiesManager:
         """
         rlist = []
         for cookie in iter(self._jar):
-            if ((domain is None or cookie.domain == domain) and
-                    (path is None or cookie.path == path)):
+            if (domain is None or cookie.domain == domain) and (
+                path is None or cookie.path == path
+            ):
                 dictionary = {
-                    'name': cookie.name,
-                    'value': cookie.value,
-                    'domain': cookie.domain,
-                    'path': cookie.path,
+                    "name": cookie.name,
+                    "value": cookie.value,
+                    "domain": cookie.domain,
+                    "path": cookie.path,
                 }
                 if cookie.expires:
-                    dictionary['expires'] = cookie.expires
+                    dictionary["expires"] = cookie.expires
                 rlist.append(dictionary)
         return rlist
 
     def output_simplecookie(self, domain=None, path=None):
         C = SimpleCookie()
         for cookie in iter(self._jar):
-            if ((domain is None or cookie.domain == domain) and
-                    (path is None or cookie.path == path)):
+            if (domain is None or cookie.domain == domain) and (
+                path is None or cookie.path == path
+            ):
                 C[cookie.name] = cookie_to_morsel(cookie)
         return C
 
@@ -123,7 +149,7 @@ class CookiesManager:
     def set_cookie(self, cookie, *args, **kwargs):
         return self._jar.set_cookie(cookie, *args, **kwargs)
 
-    def copy(self) -> 'CookiesManager':
+    def copy(self) -> "CookiesManager":
         m = CookiesManager()
         m.update(self._jar)
         return m
@@ -143,38 +169,37 @@ def morsel_to_cookie(morsel):
     """
 
     expires = None
-    if morsel['max-age']:
+    if morsel["max-age"]:
         try:
-            expires = int(time.time() + int(morsel['max-age']))
+            expires = int(time.time() + int(morsel["max-age"]))
         except ValueError:
-            raise TypeError('max-age: %s must be integer' % morsel['max-age'])
-    elif morsel['expires']:
-        expires = expires_to_number(morsel['expires'])
+            raise TypeError("max-age: %s must be integer" % morsel["max-age"])
+    elif morsel["expires"]:
+        expires = expires_to_number(morsel["expires"])
     return create_cookie(
-        comment=morsel['comment'],
-        comment_url=bool(morsel['comment']),
+        comment=morsel["comment"],
+        comment_url=bool(morsel["comment"]),
         discard=False,
-        domain=morsel['domain'],
+        domain=morsel["domain"],
         expires=expires,
         name=morsel.key,
-        path=morsel['path'],
+        path=morsel["path"],
         port=None,
-        rest={'HttpOnly': morsel['httponly']},
+        rest={"HttpOnly": morsel["httponly"]},
         rfc2109=False,
-        secure=bool(morsel['secure']),
+        secure=bool(morsel["secure"]),
         value=morsel.value,
-        version=morsel['version'] or 0,
+        version=morsel["version"] or 0,
     )
 
 
 def cookie_to_morsel(cookie):
     """Convert a Cookie object into a Morsel"""
     info = {}
-    attrs = ('version', 'domain', 'path', 'expires',
-             'secure', 'comment')
+    attrs = ("version", "domain", "path", "expires", "secure", "comment")
 
     for attr in attrs:
-        if attr == 'expires':
+        if attr == "expires":
             expires = getattr(cookie, attr)
             if expires:
                 info[attr] = expires_to_str(expires)
@@ -193,18 +218,18 @@ def create_morsel(key, value, **kwargs):
     morsel = Morsel()
     morsel.set(key, value, str(value))
     result = {
-        'version': '',
-        'domain': '',
-        'path': '/',
-        'secure': '',
-        'expires': '',
-        'comment': '',
-        'httponly': ''
+        "version": "",
+        "domain": "",
+        "path": "/",
+        "secure": "",
+        "expires": "",
+        "comment": "",
+        "httponly": "",
     }
 
     badargs = set(kwargs) - set(result)
     if badargs:
-        err = 'create_cookie() got unexpected keyword arguments: %s'
+        err = "create_cookie() got unexpected keyword arguments: %s"
         raise TypeError(err % list(badargs))
 
     morsel.update(kwargs)
@@ -217,30 +242,30 @@ def create_cookie(name, value, **kwargs):
 
     """
     result = {
-        'version': 0,
-        'name': name,
-        'value': value,
-        'port': None,
-        'domain': '',
-        'path': '/',
-        'secure': False,
-        'expires': None,
-        'discard': False,
-        'comment': None,
-        'comment_url': None,
-        'rest': {'HttpOnly': None},
-        'rfc2109': False,
+        "version": 0,
+        "name": name,
+        "value": value,
+        "port": None,
+        "domain": "",
+        "path": "/",
+        "secure": False,
+        "expires": None,
+        "discard": False,
+        "comment": None,
+        "comment_url": None,
+        "rest": {"HttpOnly": None},
+        "rfc2109": False,
     }
 
     badargs = set(kwargs) - set(result)
     if badargs:
-        err = 'create_cookie() got unexpected keyword arguments: %s'
+        err = "create_cookie() got unexpected keyword arguments: %s"
         raise TypeError(err % list(badargs))
 
     result.update(kwargs)
-    result['port_specified'] = bool(result['port'])
-    result['domain_specified'] = bool(result['domain'])
-    result['domain_initial_dot'] = result['domain'].startswith('.')
-    result['path_specified'] = bool(result['path'])
+    result["port_specified"] = bool(result["port"])
+    result["domain_specified"] = bool(result["domain"])
+    result["domain_initial_dot"] = result["domain"].startswith(".")
+    result["path_specified"] = bool(result["path"])
 
     return Cookie(**result)
--- aninja/login/loginer.py
+++ aninja/login/loginer.py
@@ -3,14 +3,10 @@ from aninja.browser import launch, BrowserClient, NinjaPage
 
 
 class Loginer:
-    url = 'Not determined'
-    login_mark = 'Not determined'
+    url = "Not determined"
+    login_mark = "Not determined"
 
-    def __init__(self,
-                 client,
-                 username: str = '',
-                 password: str = ''
-                 ):
+    def __init__(self, client, username: str = "", password: str = ""):
         self.username = username
         self.password = password
         self.client: BrowserClient = client
@@ -18,24 +14,32 @@ class Loginer:
         self.page: NinjaPage = None
 
     @classmethod
-    async def launch(cls, browser=None, cookies_manager=None, options: dict = None, username='', password='', **kwargs):
+    async def launch(
+        cls,
+        browser=None,
+        cookies_manager=None,
+        options: dict = None,
+        username="",
+        password="",
+        **kwargs
+    ):
         client = await launch(browser, cookies_manager, options, **kwargs)
         loginer = cls(client, username, password)
         loginer.page = await loginer.client.newPage()
         return loginer
 
-    def config(self,
-               username: str = '',
-               password: str = '', ):
+    def config(self, username: str = "", password: str = ""):
         self.username = username
         self.password = password
 
     async def has_logged_in(self):
         return await self.login_by_browser(only_check=True)
 
-    async def login_by_browser(self, only_check=False, qr_login: bool = False, ) -> bool:
+    async def login_by_browser(
+        self, only_check=False, qr_login: bool = False
+    ) -> bool:
         self.page: NinjaPage = await self.client.newPage()
-        await self.page.goto(self.url)
+        await self.page.goto(self.url, options={"waitUtil": "networkidle0"})
         login = bool(await self.page.check(check_flag=self.login_mark))
 
         if not only_check and not login:
@@ -44,6 +48,8 @@ class Loginer:
             else:
                 await self.login_with_password()
             login = bool(await self.page.check(check_flag=self.login_mark))
+
+        self.cookies_manager.never_expires()
         return login
 
     async def close(self):
